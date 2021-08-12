6736bb192c793a8367a6b34e92d9a193e610c4cb
Rob Eastwood <re.web.accounts@protonmail.com>
2021-08-13 07:23:30 +1000

    initial commit

---
+++ chapter_01/01/spec/sandwich_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'An ideal sandwich' do
+    it 'is delicious' do
+        sandwich = Sandwich.new('delicious', [])
+        taste = sandwich.taste
+        expect(taste).to eq('delicious')
+    end
+end
