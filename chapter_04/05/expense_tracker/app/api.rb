e665d3ea4e777ed3de9a9d729243cbeed19b42c3
Rob Eastwood <re.web.accounts@protonmail.com>
2021-08-16 07:39:02 +1000

    Add steps for chapter 04 / 05

---
+++ chapter_04/05/expense_tracker/app/api.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+require 'json'
+
+module ExpenseTracker
+    class API < Sinatra::Base
+        post '/expenses' do
+            JSON.generate('expense_id' => 42)
+        end
+        get '/expenses/:date' do
+            JSON.generate([])
+        end
+    end
+end
