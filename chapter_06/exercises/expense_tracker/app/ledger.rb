f22ff7ec3b89df4ba9ea895802fc3ab0cf1dff13
Rob Eastwood <re.web.accounts@protonmail.com>
2021-08-20 20:51:18 +1000

    Add ch 06 exercises (the first part re validation at least)
    Couldn't figure out implementing with XML and the other stuff

---
+++ chapter_06/exercises/expense_tracker/app/ledger.rb
@@ -0,0 +1,31 @@
+require_relative '../config/sequel'
+
+module ExpenseTracker
+    RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
+    class Ledger
+        def record(expense)
+            unless expense.key?('payee')
+                message = 'Invalid expense: `payee` is required'
+                return RecordResult.new(false, nil, message)
+            end
+            
+            unless expense.key?('date')
+                message = 'Invalid expense: `date` is required'
+                return RecordResult.new(false, nil, message)
+            end
+            
+            unless expense.key?('amount')
+                message = 'Invalid expense: `amount` is required'
+                return RecordResult.new(false, nil, message)
+            end
+
+            DB[:expenses].insert(expense)
+            id = DB[:expenses].max(:id)
+            RecordResult.new(true, id, nil)
+        end
+        def expenses_on(date)
+            DB[:expenses].where(date: date).all
+        end
+    end
+end
---
+++ chapter_06/exercises/expense_tracker/spec/support/db.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |c|
+    c.before(:suite) do
+        Sequel.extension :migration
+        Sequel::Migrator.run(DB, 'db/migrations')
+        DB[:expenses].truncate
+    end
+    c.around(:example, :db) do |example|
+        DB.transaction(rollback: :always) { example.run }
+    end
+end
